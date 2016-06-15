56df151d01a16b4dbe9603d764007e03a9d511f6
Taka Okunishi <okunishitaka.com@gmail.com>
2016-06-15 16:41:16 +0900

    Add koa extension

---
+++ test/with_koa_test.js
@@ -0,0 +1,45 @@
+/**
+ * Test case for withKoa.
+ * Runs with mocha.
+ */
+'use strict'
+
+const withKoa = require('../lib/with_koa.js')
+const apemanrequest = require('apemanrequest')
+const assert = require('assert')
+const co = require('co')
+
+const { Router } = withKoa
+
+describe('with-koa', () => {
+  let request = apemanrequest.create({})
+  before(() => co(function * () {
+
+  }))
+
+  after(() => co(function * () {
+
+  }))
+
+  it('With koa', () => co(function * () {
+    let port = 9881
+    let cloud = yield withKoa([
+      new Router()
+        .get('/foo/bar', (ctx) => {
+          ctx.body = 'This is bar'
+        }).routes()
+    ], {
+      port,
+      storage: `${__dirname}/../tmp/testing-cloud-koa`
+    })
+    let { statusCode, body } = yield request({
+      method: 'GET',
+      url: `http://localhost:${port}/foo/bar`
+    })
+    assert.equal(statusCode, 200)
+    assert.equal(body, 'This is bar')
+    yield cloud.close()
+  }))
+})
+
+/* global describe, before, after, it */

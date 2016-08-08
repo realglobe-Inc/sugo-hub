bec8193dfa9ff40fee5aa8330b534c0676d72845
Taka Okunishi <okunishitaka.com@gmail.com>
2016-08-08 11:52:34 +0900

    Fix browser script

--- ci/browser.js
+++ ci/browser.js
@@ -14,7 +14,7 @@ const ababelES2015 = require('ababel-es2015')
 
 apeTasking.runTasks('browser', [
   () => co(function * () {
-    let patterns = [ 'agent.js', 'constants/*.js' ]
+    let patterns = [ 'agent.js', 'constants.js' ]
     for (let pattern of patterns) {
       yield ababelES2015(pattern, {
         cwd: 'lib',

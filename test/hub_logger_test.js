cd25b2dd38d367c35fd8b295105bcdad1b666c6a
Taka Okunishi <okunishinish@gmail.com>
2016-12-27 16:45:53 +0900

    Add hub logger

---
+++ test/hub_logger_test.js
@@ -0,0 +1,27 @@
+/**
+ * Test case for hubLogger.
+ * Runs with mocha.
+ */
+'use strict'
+
+const hubLogger = require('../lib/logging/hub_logger.js')
+const assert = require('assert')
+const co = require('co')
+
+describe('hub-logger', function () {
+  this.timeout(3000)
+
+  before(() => co(function * () {
+
+  }))
+
+  after(() => co(function * () {
+
+  }))
+
+  it('Hub logger', () => co(function * () {
+
+  }))
+})
+
+/* global describe, before, after, it */
